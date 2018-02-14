7933a5e66353e133fc80de824ae0aba7057c0f53
Formation <Formation@192.168.2.87>
2018-02-14 15:09:46 +0100

    Normalisation pour jeu des tests dans jenkins

---
+++ src/main/java/petStoreSimpleTest/App.java
@@ -0,0 +1,13 @@
+package petStoreSimpleTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
